6dea50a0768136c07b40af89bac1c37e42d4bd17
bobby45800 <44036776+bobby45800@users.noreply.github.com>
2020-11-23 03:03:03 +0000

    Parser finished

--- boolean.py
+++ boolean.py
@@ -32,33 +32,37 @@ class Token:
     }
 
 
-# Evalutates not expressions
+# Evalutates not symbols
 class NotExpression:
-    def __init__(self, sub_expr):
-        self.sub_expr = sub_expr
+    def __init__(self, sub_symbol):
+        self.sub_symbol = sub_symbol
 
-    def evaluate(self):
-        return not self.sub_expr.evaluate()
+    def evaluate(self, context):
+        return not self.sub_symbol.evaluate(context)
 
 
-# Evaluates and expressions
+# Evaluates and terms
 class AndExpression:
-    def __init__(self, sub_expr_left, sub_expr_right):
-        self.sub_expr_left = sub_expr_left
-        self.sub_expr_right = sub_expr_right
+    def __init__(self, and_list):
+        self.and_list = and_list
 
-    def evaluate(self):
-        return self.sub_expr_left.evaluate() and self.sub_expr_right.evaluate()
+    def evaluate(self, context):
+        for term in self.and_list:
+            if not term.evaluate(context):
+                return False
+        return True
 
 
 # Evaluates or expressions
 class OrExpression:
-    def __init__(self, sub_expr_left, sub_expr_right):
-        self.sub_expr_left = sub_expr_left
-        self.sub_expr_right = sub_expr_right
+    def __init__(self, expression_list):
+        self.expression_list = expression_list
 
-    def evaluate(self):
-        return self.sub_expr_left.evaluate() or self.sub_expr_right.evaluate()
+    def evaluate(self, context):
+        for expr in self.expression_list:
+            if expr.evaluate(context):
+                return True
+        return False
 
 
 # Evaluates parentheses in the expression
@@ -66,11 +70,11 @@ class ParenthesizedSymbol:
     def __init__(self, sub_expr):
         self.sub_expr = sub_expr
     
-    def evaluate(self):
-        return self.sub_expr.evaluate()
+    def evaluate(self, context):
+        return self.sub_expr.evaluate(context)
 
 
-# Evaluates any variables in the expression 
+# Evaluates any variables
 class VariableSymbol:
     def __init__(self, letter):
         self.letter = letter
@@ -79,12 +83,21 @@ class VariableSymbol:
         return context[self.letter]
 
 
+# Evaluates any literals
+class LiteralSymbol:
+    def __init__(self, literal):
+        self.literal = literal
+    
+    def evaluate(self, context):
+        return self.literal
+
+
 # Parses each section of the expression into a usable state
 class Parser:
     def __init__(self, text):
         self.text = text.replace(' ', '') # Removes whitespace
         self.pos = 0
-        self.variables = set()
+        self.variables = set() # Tracks all the variables in the expression
 
     # Reads the tokens of the expression by iterating through the position
     def consume_token(self):
@@ -110,52 +123,115 @@ class Parser:
         ret = self.consume_token()
         self.pos = prevPos
         return ret
-    
-    # Returns any varianles in the expression
-    def parse_variable_symbol(self):
-        name = self.consume_token()
-        self.variables.add(name)
-        return VariableSymbol(name)
-
-    # 
-    def parse_expression(self):
-        terms = []
-        terms.append(self.parse_term())
-        token = self.consume_token()
-        while token == Token.TOKEN_ALTERNATIVES['+']:
-            terms.append(self.parse_term())
-            token = self.consume_token()
-        return terms
-
-    # 
-    def parse_term(self):
-        terms = []
-        terms.append(self.parse_term())
-        token = self.consume_token()
-        while token == Token.TOKEN_ALTERNATIVES['.']:
-            terms.append(self.parse_term())
-            token = self.consume_token()
-        return terms
-
-    # Errors if parentheses missing,
-    # Returns the expression within the parentheses
-    def parse_parenthesized_symbol(self):
-        if self.consume_token() != '(':
-            raise Exception('Incorrect syntax')
-        ret = ParenthesizedSymbol(self.parse_expression())
-        if self.consume_token() != ')':
-            raise Exception('Incorrect syntax')
-        return ret
 
     # Parsed expression
     def parse(self):
-        ret = self.parse_expression()
+        ret = self.parse_or()
         if self.consume_token() is not None:
             raise Exception('Parse error')
         return ret
 
+    # Returns the or of expression
+    def parse_or(self):
+        terms = []
+        terms.append(self.parse_and())
+        token = self.peek_token()
+        while token == "+":
+            token = self.consume_token()
+            terms.append(self.parse_and())
+            token = self.peek_token()
+        ret = OrExpression(terms)
+        return ret
 
-parser = Parser(text="A + B")
+    # Returns the and term of expression
+    def parse_and(self):
+        terms = []
+        terms.append(self.parse_symbol())
+        token = self.peek_token()
+        while token == ".":
+            token = self.consume_token()
+            terms.append(self.parse_symbol())
+            token = self.peek_token()
+        ret = AndExpression(terms)
+        return ret
+
+    # Returns the expression within the parentheses
+    # Errors if either parentheses missing
+    def parse_parenthesized_symbol(self):
+        token = self.consume_token()
+        if not token == "(":
+            raise Exception('Missing (')
+        ret = ParenthesizedSymbol(self.parse_or())
+        token = self.consume_token()
+        if not token == ")":
+            raise Exception('Missing )')
+        return ret
+
+    # If next pos is open bracket then parse parenthesis
+    # If next pos is not then check if not
+    # If next pos is literal then set true or false
+    # If next pos is variable then parse variable symbol
+    def parse_symbol(self):
+        token = self.peek_token()
+        if token == "(":
+            return self.parse_parenthesized_symbol()
+        elif token == "!":
+            return self.parse_not()
+        elif token == "1" or token == "0":
+            return self.parse_literal()
+        else:
+            return self.parse_variable_symbol()
+
+    # Returns the not of the expression
+    def parse_not(self):
+        token = self.consume_token()
+        if not token == "!":
+            raise Exception('Invalid syntax')
+        sub_symbol = self.parse_symbol()
+        ret = NotExpression(sub_symbol)
+        return ret
+
+    # Returns any variables in the expression/term
+    # Raises an error if the variable is not text
+    def parse_variable_symbol(self):
+        name = self.consume_token()
+        if not name.isalpha():
+            raise Exception('Variable not detected')
+        self.variables.add(name)
+        return VariableSymbol(name)
+
+    # Returns if literal is true or false
+    def parse_literal(self):
+        token = self.consume_token()
+        if not token == "1" and not token == "0":
+            raise Exception('Incorrect syntax')
+        if token == "1":
+            literal = True
+        else:
+            literal = False
+        ret = LiteralSymbol(literal)
+        return ret
+
+
+parser = Parser(text="!(((A+B).C).0)")
 ast = parser.parse()
-# Iterate through context to get every part of it
-context = {'a': False, 'b': True}
+# generate the context variables from parser.variables.
+# e.g. 
+# a | b
+# 0 | 0
+# 0 | 1
+# 1 | 0
+# 1 | 1
+# or
+# e | x | i
+# 0 | 0 | 0
+# 0 | 1 | 0
+# 1 | 0 | 0
+# 1 | 1 | 0
+# 0 | 0 | 1
+# 0 | 1 | 1
+# 1 | 0 | 1
+# 1 | 1 | 1
+context = {'A': False, 'B': True, 'C': True}
+print(ast.evaluate(context))
+print(parser.variables)
